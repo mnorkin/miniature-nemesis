11845e0162a729e98b26d136188439633390a070
Vidmantas Žemaitis <pilkasnamas@gmail.com>
2013-04-16 23:41:40 +0300

    casperjs.test update + new tests

--- Django/prototype/static/js/casperjs.test.js
+++ Django/prototype/static/js/casperjs.test.js
@@ -1,49 +1,76 @@
-var casper = require('casper').create();
+var casper = require('casper').create({logLevel: "debug"});
+//var test_host = 'http://127.0.0.1:8000/';
+var test_host = 'http://baklazanas.lt/';
 
-casper.start('http://baklazanas.lt/', function() {
+/** Test search **/
+casper.start(test_host, function() {
 
     this.test.assertEvalEqual(function() {
-        return document.querySelectorAll('ul.search_res li').length
-    }, 0, 'Search box results are hidden'
+            return document.querySelectorAll('ul.search_res li').length
+        }, 0, 'Search box results are empty'
     );
 
     this.test.assertExists(
-        'form[method="get"]',
+        '.search form[method="get"]',
         'Search box form found'
     );
 
-    this.sendKeys('input[name="q"]', 'a');
+    this.sendKeys('#search_inp', 'a');
+    casper.waitFor(
+        function(){
+            return this.evaluate(function() {
+               return document.querySelectorAll('ul.search_res li').length == 12;
+            });
+        }, 
+        function(){
+            this.test.assertEvalEqual(function() {
+                return document.querySelectorAll('ul.search_res li').length
+            }, 12, 'Search box results are 12 entries');
+        }
+    );
 
 });
 
-casper.then(function() {
 
-    this.test.assertEvalEqual(function() {
-        return document.querySelectorAll('ul.search_res li').length;
+/** Test sp500 popup **/
+casper.then(function() {  
+
+    this.test.assert(document.querySelectorAll('#popup .list a').length == 0,  'Popup content is empty');
+
+    this.click('a.sp500');
+    casper.waitFor(
+        function(){
+            return this.evaluate(function() {
+               return document.querySelectorAll('#popup .list a').length >= 500;
+            });
+        }, 
+        function(){
+            this.test.assertEval(function(){
+                return document.querySelectorAll('#popup .list a').length >= 500
+            }, 'Popup content is more or equal 500 entries');
         },
-        12,
-        'Search box results is equal to one ticker and title'
+        function(){ this.test.assert(false, 'Popup content is more or equal 500 entries')}
     );
-
-    this.test.assertEvalEqual(function() {
-        return document.querySelector('div[id="popup"]').style['display']
-        },
-        '',
-        'Popup is hidden'
-    );
-
-    this.click('a[class="sp500"]');
-
 });
 
+
+
+/** Test title target price blocks **/
 casper.then(function() { 
 
-    this.test.assertEvalEqual(function() {
-            return document.querySelector('div[id="popup"]').style['display']
-        },
-        'block',
-        'Popup is displayed'
-    );
+    this.test.assertEval(function(){ return document.querySelectorAll('.grid #target-price-list .entry').length >= 3},  
+        'Title target price list blocks 3 or more');
+
+    this.test.assertEval(function(){ return document.querySelectorAll('.grid #target-price-list .bar svg').length >= 9},  
+        'Title target prices list blocks svg 9 or more');
+
+    // change to list view
+    this.click('a.toggle');
+    this.test.assertEval(function(){ return document.querySelectorAll('.list #target-price-list .entry').length >= 3},  
+        'Title target price grid blocks 3 or more');
+
+    this.test.assertEval(function(){ return document.querySelectorAll('.list #target-price-list .bar svg').length >= 9},  
+        'Title target prices grid blocks svg 9 or more');
 
 });
 
